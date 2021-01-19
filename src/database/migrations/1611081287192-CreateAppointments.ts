2c79f926e72c572bb749f16e0f098a7c726a79e8
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-01-19 16:38:03 -0300

    Refatora o código, adequando com a comunicação com o banco de dados e retirando o armazenamento em memória da aplicação.,

--- src/database/migrations/1611081287192-CreateAppointments.ts
+++ src/database/migrations/1611081287192-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1611081287192
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
