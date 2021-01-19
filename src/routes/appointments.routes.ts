d29573836999800aa38f9ca707509ed6f95c0265
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-01-19 14:31:09 -0300

    Adiciona as camadas da aplicação (repository, services e routes), assim como os pacotes de validação de dados e criação de UUIDs para o app. Ainda armazenando em memória.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,36 @@
+import { parseISO } from 'date-fns';
 import { Router } from 'express';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+
+  return res.json(appointments);
+});
 
 appointmentsRouter.post('/', (req, res) => {
-  return res.json({ message: 'minha rota de appointments' });
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date); // transformando dados
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
 });
 
 export default appointmentsRouter;
