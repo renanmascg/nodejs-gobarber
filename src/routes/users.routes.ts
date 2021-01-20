35ef775c898ff99e1e7a62e7aa089fce513d0ce5
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-01-20 11:41:04 -0300

    Adiciona upload de imagem, salvando localmente a imagem.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -24,5 +28,16 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    console.log(req.file);
+
+    return res.json({ ok: true });
+  },
+);
 
 export default usersRouter;
