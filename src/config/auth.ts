8bc1db5536fa64e46b8be9a6f7a12c9dd97bd884
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-01-20 11:16:02 -0300

    Adiciona arquivo de configs dentro da aplicação e configura o middleware de autenticação do mesmo, permitindo rotas autenticadas.

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '7073fd6f869e00471429389b47a7469a',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { getRepository } from 'typeorm';
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface RequestDTO {
@@ -31,9 +32,11 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '7073fd6f869e00471429389b47a7469a', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
